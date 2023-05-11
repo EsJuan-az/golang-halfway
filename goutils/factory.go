09e3d395a4c4e9c254b98c030402454898dd9005
JuanEs-az <juaneste687@gmail.com>
2023-05-11 14:14:52 -0500

    Clase 10-12: Interfaces y Abstract Factory

---
+++ goutils/factory.go
@@ -0,0 +1,86 @@
+package goutils
+
+import "fmt"
+
+//SMS Email
+type INotificacionFactory interface{
+	SendNotification()
+	GetSender() ISender
+}
+type ISender interface{
+	GetSenderMethod() string
+	GetSenderChannel() string
+}
+
+
+
+type SMSNotification struct{
+	sender SMSNotificationSender
+}
+func (SMSNotification) SendNotification(){
+	fmt.Println("Sending Notification via SMS")
+}
+func (s *SMSNotification) GetSender() ISender{
+	return s.sender
+}
+type SMSNotificationSender struct{
+
+}
+func (SMSNotificationSender) GetSenderMethod()string{
+	return "SMS"
+}
+func (SMSNotificationSender) GetSenderChannel()string{
+	return "Twilio"
+}
+
+
+
+type EmailNotification struct{
+	sender EmailNotificationSender
+}
+func (EmailNotification) SendNotification(){
+	fmt.Println("Sending Notification via Email")
+}
+func (e *EmailNotification) GetSender()	ISender{
+	return e.sender
+}
+type EmailNotificationSender struct{
+
+}
+func (EmailNotificationSender) GetSenderMethod()string{
+	return "Email"
+}
+func (EmailNotificationSender) GetSenderChannel()string{
+	return "SES"
+}
+
+
+func getNotificationFactory(notification string) (INotificacionFactory, error){
+	if notification == "SMS"{
+		return &SMSNotification{}, nil
+	}
+	if notification == "Email"{
+		return &EmailNotification{}, nil
+	}
+	return nil, fmt.Errorf("didn't match with any notification type")
+}
+func sendNotification(f INotificacionFactory){
+	f.SendNotification()
+}
+func getMethod(f INotificacionFactory){
+	fmt.Println(f.GetSender().GetSenderMethod())
+}
+func Factory(){
+	smsFactory, err1 := getNotificationFactory("SMS")
+	emailFactory, err2 := getNotificationFactory("Email")
+	if err1 != nil || err2 !=  nil{
+		fmt.Println("Ha ocurrido un error :o")
+	}else{
+		fmt.Println("Con SMS")
+		sendNotification(smsFactory)
+		getMethod(smsFactory)
+		fmt.Println("Con Email")
+		sendNotification(emailFactory)
+		getMethod(emailFactory)
+	}
+}
---
+++ goutils/interface.go
@@ -0,0 +1,49 @@
+package goutils
+
+import "fmt"
+
+type PrintInfo interface{
+	getInfo() string
+}
+func printInfo(pi PrintInfo){
+	fmt.Println(pi.getInfo())
+}
+type TemporaryEmployee struct{
+	Person
+	BaseEmployee
+	endDate string
+}
+func (te *TemporaryEmployee) getInfo() string{
+	return fmt.Sprintf("%d) %s (%d años de edad): Trabaja hasta %s",te.id, te.name, te.age, te.endDate)
+}
+func NewTemporary(id int, name string, age int, endDate string)*TemporaryEmployee{
+	te := &TemporaryEmployee{}
+	te.id = id
+	te.name = name
+	te.age = age
+	te.endDate = endDate
+	return te
+
+}
+type FullTimeEmployee struct{
+	Person
+	BaseEmployee
+	taxRate int
+}
+func (fte *FullTimeEmployee) getInfo() string{
+	return fmt.Sprintf("%d) %s (%d años de edad): Índice de impuestos %d",fte.id, fte.name, fte.age, fte.taxRate)
+}
+func NewFullTime(id int, name string, age, taxRate int)*FullTimeEmployee{
+	fte := &FullTimeEmployee{}
+	fte.id = id
+	fte.name = name
+	fte.age = age
+	fte.taxRate = taxRate
+	return fte
+}
+func Interface(){
+	t := NewTemporary(1, "Pepe", 15, "1 de Mayo")
+	ft := NewFullTime(2, "Pedro", 51, 10)
+	printInfo(t)
+	printInfo(ft)
+}
